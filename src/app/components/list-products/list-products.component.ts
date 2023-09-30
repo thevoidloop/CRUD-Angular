67c3f00fced64b071a665a33062ba4cad236885f
Daniel Marroquin <marroquin_daniel@outlook.com>
2023-09-29 23:13:01 -0600

    Iteramos datos en nuestra tabla y agregamos botones de editar, eliminar y agregar productos

--- src/app/components/list-products/list-products.component.ts
+++ src/app/components/list-products/list-products.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/interfaces/product';
 
 @Component({
   selector: 'app-list-products',
   templateUrl: './list-products.component.html',
   styleUrls: ['./list-products.component.css']
 })
-export class ListProductsComponent {
+
+export class ListProductsComponent implements OnInit{
+  
+  listProducts: Product[] = [
+    { name: 'Coca Cola', description: 'Bebidas con azucar', price: 4, stock: 200},
+    { name: 'Corona', description: 'Bebidas con alcohol', price: 5, stock: 300},
+  ]
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
+
+
 
 }
